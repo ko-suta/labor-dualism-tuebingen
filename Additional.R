1226192a90200e59ee965c0130b9ccc330ef155d
Kota <k.ovsiannikov@gmail.com>
2021-11-25 16:36:48 +0900

    clean up

---
+++ Additional.R
@@ -0,0 +1,10 @@
+library(pagedown)
+library(xaringan)
+library(xaringanBuilder)
+library(here)
+
+------------------
+
+# build_pdf(here("slides", "Dual-Market-OECD", "Labor-Market-Dualism-OECD.Rmd"))
+
+# chrome_print("https://dual-market.netlify.app/slides/dual-market-oecd/labor-market-dualism-oecd", output = "Intro.pdf", format = "pdf")
