0f91681b312b54b30ce1073639a19dcdc652d60a
Kostya <k.ovsiannikov@gmail.com>
2021-12-15 15:33:50 +0900

    edit lecture 4

--- PrintFunctions.R
+++ PrintFunctions.R
@@ -5,6 +5,6 @@ library(here)
 
 ------------------
 
-# build_pdf(here("slides", "Dual-Market-OECD", "Labor-Market-Dualism-OECD.Rmd"))
+build_pdf(here("slides", "Dual-Market-OECD", "Labor-Market-Dualism-OECD.Rmd"))
 
 # chrome_print("https://dual-market.netlify.app/slides/dual-market-oecd/labor-market-dualism-oecd", output = "Intro.pdf", format = "pdf")
